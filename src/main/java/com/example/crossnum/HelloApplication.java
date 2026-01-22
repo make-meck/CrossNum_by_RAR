56441f72dfbd53f48bb0fc253cc5f510ea739c50
Mickz <211024171+make-meck@users.noreply.github.com>
2026-01-22 14:02:35 +0800

    --push mo yan teh

--- src/main/java/com/example/crossnum/HelloApplication.java
+++ src/main/java/com/example/crossnum/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("starting_page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 1550, 800);
 
         scene.getStylesheets().add(
                 getClass().getResource("style.css").toExternalForm()
