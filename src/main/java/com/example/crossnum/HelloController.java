044eabd5688ee66f32270e9d51962e9bc625ac20
chriscc77 <chrisancabrera077@gmail.com>
2026-01-25 20:59:51 +0800

    hintfndfuw

--- src/main/java/com/example/crossnum/HelloController.java
+++ src/main/java/com/example/crossnum/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.crossnum;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    Button hint;
+    @FXML
+    Button eraser;
+    @FXML
+    Button pen;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onHintClick(){
+    }
+    @FXML
+    protected void onEraserClick(){
+    }
+    @FXML
+    protected void onPenClick(){
     }
 }
