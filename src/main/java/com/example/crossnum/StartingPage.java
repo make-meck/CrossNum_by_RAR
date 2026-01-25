a3a9e24894c1aa9f989c7f58cc36507efba09dde
kirsten265 <cruz.kirsten265@gmail.com>
2026-01-26 00:00:48 +0800

    w back button in level_selection

--- src/main/java/com/example/crossnum/StartingPage.java
+++ src/main/java/com/example/crossnum/StartingPage.java
@@ -7,25 +7,40 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class StartingPage {
 
-    // 1. Inject the StackPane
-    @FXML
-    private StackPane main_stack;
-
-    @FXML
-    private BorderPane root_pane;
-
-    @FXML
-    private Button play_button;
+    @FXML private StackPane main_stack;
+    @FXML private BorderPane root_pane;
+    @FXML private Button play_button;
+    @FXML private Button help_button;
+    @FXML private Button easy_button;
+    @FXML private Button medium_button;
+    @FXML private Button hard_button;
 
     @FXML
     private void playClick() throws IOException {
         FXMLLoader playLoader = new FXMLLoader(getClass().getResource("level_selection.fxml"));
         Parent overlay = playLoader.load();
+
+        overlay.setOnMouseClicked(event -> {
+            if (event.getTarget() == overlay) {
+                main_stack.getChildren().remove(overlay);
+            }
+        });
+
         main_stack.getChildren().add(overlay);
         StackPane.setAlignment(overlay, Pos.BOTTOM_CENTER);
+    }
+
+    @FXML
+    private void howToPlay() throws IOException {
+        FXMLLoader helpLoader = new FXMLLoader(getClass().getResource("mechanics_page.fxml"));
+        Stage stage = (Stage) help_button.getScene().getWindow();
+        Parent root = helpLoader.load();
+        stage.getScene().setRoot(root);
     }
 }
