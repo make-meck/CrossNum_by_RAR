8cf786ff11970c71755b019a6ef8b496f0b83fac
chriscc77 <chrisancabrera077@gmail.com>
2026-02-02 22:13:34 +0800

    toggle adjustments

--- src/main/java/com/example/crossnum/HelloApplication.java
+++ src/main/java/com/example/crossnum/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start_page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("easy_page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
 
         scene.getStylesheets().add(
--- src/main/java/com/example/crossnum/HelloController.java
+++ src/main/java/com/example/crossnum/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.crossnum;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class HelloController {
     @FXML
@@ -13,6 +18,22 @@ public class HelloController {
     Button eraser;
     @FXML
     Button pen;
+    @FXML
+    Circle toggleCircle;
+    @FXML
+    ImageView penImage;
+    @FXML
+    ImageView eraserImage;
+    private boolean penMode = true;
+
+    private final Image blackEraser =
+            new Image(getClass().getResource("eraser.png").toExternalForm());
+    private final Image whiteEraser =
+            new Image(getClass().getResource("white_eraser.png").toExternalForm());
+    private final Image whitePen =
+            new Image(getClass().getResource("white_pen.png").toExternalForm());
+    private final Image blackPen =
+            new Image(getClass().getResource("pen.png").toExternalForm());
 
 
     @FXML
@@ -23,10 +44,33 @@ public class HelloController {
     @FXML
     protected void onHintClick(){
     }
+
     @FXML
-    protected void onEraserClick(){
+    private void onPenClick() {
+        penMode = true;
+        updateToggle();
+        System.out.println("PEN");
     }
+
     @FXML
-    protected void onPenClick(){
+    private void onEraserClick() {
+        penMode = false;
+        updateToggle();
+        System.out.println("ERASER");
+    }
+
+    private void updateToggle() {
+        if (penMode) {
+            toggleCircle.setTranslateX(75);
+            penImage.setImage(blackPen);
+            eraserImage.setImage(whiteEraser);
+        } else {
+            toggleCircle.setTranslateX(0);
+            eraserImage.setImage(blackEraser);
+            penImage.setImage(whitePen);
+        }
+        TranslateTransition tt = new TranslateTransition(Duration.millis(500), toggleCircle);
+        tt.setToX(penMode ? 75 : 0);
+        tt.play();
     }
 }
