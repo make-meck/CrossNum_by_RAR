24e07a40be0d39184641e22c44f95d6eea442601
Mickz <211024171+make-meck@users.noreply.github.com>
2026-02-12 04:43:47 +0800

    -- hardpage update

---
+++ src/main/java/com/example/crossnum/HardPageController.java
@@ -0,0 +1,31 @@
+package com.example.crossnum;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class HardPageController {
+    @FXML private Button backbuttonHard;
+    @FXML private Button hint;
+
+    @FXML
+    private void backbutton(ActionEvent event) {
+        try {
+            FXMLLoader backbuttonLoader = new FXMLLoader(getClass().getResource("start_page.fxml"));
+            Stage stage = (Stage) backbuttonHard.getScene().getWindow();
+            Parent root = backbuttonLoader.load();
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    protected void onHintClick(){
+    }
+}
